17cb9e03496c77ad8cc1694db88a8ae3373e21f6
WilliamSongLiu <williamsongliu@gmail.com>
2025-03-09 22:02:35 -0700

    Update test2.py

--- test2.py
+++ test2.py
@@ -4,14 +4,10 @@ import os
 from jsbgym import visualiser
 import logging
 
-# Configure logging and patch the visualiser's logger
+# Weird necessary fix
 logging.basicConfig(level=logging.INFO)
 visualiser.gym.logger = logging.getLogger('jsbgym')
 
-# Set FlightGear environment variables
-os.environ['FG_ROOT'] = 'C:/Users/notan/FlightGear/Downloads/fgdata_2024_1'
-
-# Create and initialize the environment
 env = gym.make("C172-HeadingControlTask-Shaping.STANDARD-FG-v0", render_mode="flightgear")
 observation, info = env.reset()
 env.render()
