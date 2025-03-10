8cb20f383b07948003f3880e516f61bee960a49c
WilliamSongLiu <williamsongliu@gmail.com>
2025-03-09 22:10:11 -0700

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,5 +1,6 @@
 import jsbgym
 import gymnasium as gym
+import numpy as np
 
 # Weird necessary fix
 from jsbgym import visualiser
@@ -9,4 +10,8 @@ visualiser.gym.logger = logging.getLogger('jsbgym')
 
 env = gym.make("C172-HeadingControlTask-Shaping.STANDARD-FG-v0", render_mode="flightgear")
 observation, info = env.reset()
-env.render()
+
+terminated = False
+while not terminated:
+    observation, reward, terminated, truncated, info = env.step(np.array([0, 0, 0]))
+    env.render()
