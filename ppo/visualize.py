454153617a1c5998942520414746cf7f37cfe2e7
WilliamSongLiu <williamsongliu@gmail.com>
2025-03-12 14:18:03 -0700

    Update visualize.py

--- ppo/visualize.py
+++ ppo/visualize.py
@@ -1,4 +1,3 @@
-
 import gymnasium as gym
 import jsbgym
 import os
@@ -11,24 +10,26 @@ import gym_make
 from tasks import LandingTask
 gym_make.main()
 
-
 # Weird necessary fix
 from jsbgym import visualiser
 import logging
 logging.basicConfig(level=logging.INFO)
 visualiser.gym.logger = logging.getLogger('jsbgym')
-env = gym.make("C172-LandingTask-Shaping.STANDARD-FG-v0", render_mode="flightgear")
+
+env = gym.make("C172-HeadingControlTask-Shaping.STANDARD-FG-v0", render_mode="flightgear")
 
 env.reset()
-model=PPO.load(os.path.join("train", "model"))
+model = PPO.load("model")
 env.render()
+
 for episode in range(1, 6):
     obs, _ = env.reset()
     done = False
     total_reward = 0
     while not done:
         action, _ = model.predict(obs)
-        obs, reward, done, _, info = env.step(action)
+        obs, reward, terminated, truncated, info = env.step(action)
+        done = terminated or truncated
         total_reward += reward
-    print("Total Reward for episode {} is {}".format(episode, total_reward))
+    print(f"Total Reward for episode {episode} is {total_reward}")
 env.close()
