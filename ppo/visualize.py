e7af1a606b44f43a32b27400035d4cd991714616
WilliamSongLiu <williamsongliu@gmail.com>
2025-03-16 22:07:15 -0700

    Update visualize.py

--- ppo/visualize.py
+++ ppo/visualize.py
@@ -2,21 +2,20 @@ import gymnasium as gym
 import jsbgym
 import os
 from stable_baselines3 import PPO
+from sb3_contrib import RecurrentPPO
 from stable_baselines3.common.callbacks import BaseCallback
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.vec_env import DummyVecEnv, VecFrameStack
 import numpy as np
-import tensorflow as tf
 import torch
-from gym.wrappers import HumanRendering
 import gym_make
 from tasks import AltitudeTask
 gym_make.main()
 from stable_baselines3.common.policies import obs_as_tensor
 
-def predict_proba(model, state):
+def predict_proba(model, state, lstm_states, episode_starts):
     obs = model.policy.obs_to_tensor(state)[0]
-    dis = model.policy.get_distribution(obs)
+    dis = model.policy.get_distribution(obs, lstm_states, episode_starts)
     #print(dis)
     probs = dis.distribution.mean
     std_np = dis.distribution.stddev.detach().numpy()
@@ -30,10 +29,10 @@ import logging
 logging.basicConfig(level=logging.INFO)
 visualiser.gym.logger = logging.getLogger('jsbgym')
 
-env = gym.make("C172-CustomTurnHeadingControlTask-Shaping.EXTRA_SEQUENTIAL-FG-v0", render_mode="graph")
+env = gym.make("C172-CustomTurnHeadingControlTask-Shaping.EXTRA_SEQUENTIAL-FG-v0", render_mode="flightgear")
 
 env.reset()
-model = PPO.load("model")
+model = RecurrentPPO.load("model")
 #model = PPO.load(os.path.join("train", "best_model_200000"))
 env.render()
 
@@ -43,14 +42,16 @@ while True:
     obs, _ = env.reset()
     done = False
     total_reward = 0
+    lstm_states = None
+    episode_starts = np.ones((1,))
     while not done:
-        action, _ = model.predict(obs)
+        action, lstm_states = model.predict(obs, lstm_states, episode_starts)
         #print(action)
         #for i in range(30):
         #    action1, _ = model.predict(obs)
         #    action+= action1
         #action /=31
-        action = np.clip(predict_proba(model, obs)[0], min=-1, max=1)
+        # action = np.clip(predict_proba(model, obs, lstm_states, episode_starts)[0], min=-1, max=1)
         #print(action)
         obs, reward, terminated, truncated, info = env.step(action)
         #print(obs[0])
